1a8fff862702833a4f691af1f6329cce9df7073b
Natália Schardosim <natalia.schardosim@hotmail.com>
2024-10-16 13:03:26 +0200

    improved structure of libft files

--- Makefile
+++ Makefile
@@ -1,17 +1,40 @@
-SRCS = ft_free_matrix.c ft_matrixdup.c ft_isalpha.c ft_isdigit.c ft_isalnum.c ft_isascii.c ft_isprint.c ft_strlen.c ft_memset.c ft_bzero.c ft_memcpy.c ft_memmove.c ft_strlcpy.c ft_strlcat.c ft_toupper.c ft_tolower.c ft_strrchr.c ft_strchr.c ft_strncmp.c ft_memchr.c ft_memcmp.c ft_strnstr.c ft_atoi.c ft_calloc.c ft_strdup.c ft_substr.c ft_strjoin.c ft_strtrim.c ft_split.c ft_itoa.c ft_strmapi.c ft_striteri.c ft_putchar_fd.c ft_putstr_fd.c ft_putendl_fd.c ft_putnbr_fd.c
+CC = cc
+RM = rm -f
+CFLAGS = -Wall -Wextra -Werror
 
-BONUS = ft_lstnew.c ft_lstadd_front.c ft_lstsize.c ft_lstlast.c ft_lstadd_back.c ft_lstdelone.c ft_lstclear.c ft_lstiter.c ft_lstmap.c
+SRCS = .
 
-OBJS_S = $(SRCS:.c=.o)
-OBJS_B = $(BONUS:.c=.o)
+MX_DIR = matrix
+MATRIX = $(MX_DIR)/ft_free_matrix.c $(MX_DIR)/ft_matrixdup.c $(MX_DIR)/ft_split.c
+
+CH_DIR = char_checks
+CHAR_CH = $(CH_DIR)/ft_isalpha.c $(CH_DIR)/ft_isdigit.c $(CH_DIR)/ft_isalnum.c $(CH_DIR)/ft_isascii.c $(CH_DIR)/ft_isprint.c $(CH_DIR)/ft_toupper.c $(CH_DIR)/ft_tolower.c \
+	$(CH_DIR)/ft_isspace.c
+
+S_DIR = string
+STRING = $(S_DIR)/ft_strlen.c $(S_DIR)/ft_strlcpy.c $(S_DIR)/ft_strlcat.c $(S_DIR)/ft_strrchr.c $(S_DIR)/ft_strchr.c $(S_DIR)/ft_strncmp.c $(S_DIR)/ft_strnstr.c \
+	$(S_DIR)/ft_strdup.c $(S_DIR)/ft_substr.c $(S_DIR)/ft_strjoin.c $(S_DIR)/ft_strtrim.c $(S_DIR)/ft_strmapi.c $(S_DIR)/ft_striteri.c $(S_DIR)/ $(S_DIR)/
+
+MEM_DIR = memory
+MEM = $(MEM_DIR)/ft_memset.c $(MEM_DIR)/ft_bzero.c $(MEM_DIR)/ft_memcpy.c $(MEM_DIR)/ft_memmove.c $(MEM_DIR)/ft_memchr.c $(MEM_DIR)/ft_calloc.c $(MEM_DIR)/ $(MEM_DIR)/ $(MEM_DIR)/
+
+CV_DIR = conversion
+CONV = $(CV_DIR)/ft_atoi.c $(CV_DIR)/ft_itoa.c $(CV_DIR)/ $(CV_DIR)/ $(CV_DIR)/ $(CV_DIR)/ $(CV_DIR)/ $(CV_DIR)/ $(CV_DIR)/ $(CV_DIR)/ $(CV_DIR)/
+
+FD_DIR = fd_io
+FDIO = $(FD_DIR)/ft_putchar_fd.c $(FD_DIR)/ft_putendl_fd.c $(FD_DIR)/ft_putnbr_fd.c $(FD_DIR)/ft_putstr_fd.c
+
+LS_DIR = list
+LIST = $(LS_DIR)/ft_lstnew.c $(LS_DIR)/ft_lstadd_front.c $(LS_DIR)/ft_lstsize.c $(LS_DIR)/ft_lstlast.c $(LS_DIR)/ft_lstadd_back.c $(LS_DIR)/ft_lstdelone.c $(LS_DIR)/ft_lstclear.c \
+	$(LS_DIR)/ft_lstiter.c $(LS_DIR)/ft_lstmap.c
+
+# Combine all source files
+SRCS = $(MATRIX) $(CHAR_CH) $(STRING) $(MEM) $(CONV) $(FDIO) $(LIST)
+OBJS = $(SRCS:%.c=$(OBJDIR)/%.o)
 
 GNL_DIR = get_next_line
 GNL_SRCS = $(GNL_DIR)/get_next_line.c $(GNL_DIR)/get_next_line_utils.c
 GNL_OBJS = $(GNL_SRCS:$(GNL_DIR)/%.c=$(OBJDIR)/%.o)
-
-CC = cc
-RM = rm -f
-CFLAGS = -Wall -Wextra -Werror
 
 PRINTF_DIR = ./ft_printf
 PRINTF_LIB = $(PRINTF_DIR)/libftprintf.a
@@ -21,11 +44,8 @@ OBJDIR = obj
 
 all: $(NAME)
 
-bonus: $(OBJS_B) $(OBJS_S) $(GNL_OBJS)
-	ar r $(NAME) $(OBJS_B) $(OBJS_S) $(GNL_OBJS)
-
-$(NAME): $(OBJS_S) $(GNL_OBJS) $(PRINTF_LIB)
-	ar r $(NAME) $(OBJS_S) $(GNL_OBJS)
+$(NAME): $(OBJS) $(GNL_OBJS) $(PRINTF_LIB)
+	ar r $(NAME) $(OBJS) $(GNL_OBJS)
 
 $(PRINTF_LIB):
 	$(MAKE) -C $(PRINTF_DIR)
@@ -42,11 +62,12 @@ $(OBJDIR)/%.o: $(GNL_DIR)/%.c | $(OBJDIR)
 	$(CC) $(CFLAGS) -c $< -o $@
 
 clean:
-	$(RM) $(OBJS_S) $(OBJS_B) $(GNL_OBJS)
+	$(RM) -rf $(OBJDIR)/*.o
 	$(MAKE) -C $(PRINTF_DIR) fclean
 
+# Full clean
 fclean: clean
-	$(RM) $(NAME)
+	$(RM) -f $(NAME)
 	$(MAKE) -C $(PRINTF_DIR) fclean
 
 re: fclean all
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: nataliaschardosim <nataliaschardosim@st    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/12 19:23:45 by nataliascha       #+#    #+#             */
-/*   Updated: 2024/10/16 12:09:05 by nataliascha      ###   ########.fr       */
+/*   Updated: 2024/10/16 12:58:20 by nataliascha      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,56 +22,85 @@
 # include "ft_printf/ft_printf.h"
 # include "get_next_line/get_next_line.h"
 
+// List structure definition
 typedef struct s_list
 {
 	void			*content;
 	struct s_list	*next;
-}					t_list;
+}	t_list;
 
-int		ft_isalpha(int c);
-int		ft_isdigit(int c);
-int		ft_isalnum(int c);
-int		ft_isascii(int c);
-int		ft_isprint(int c);
-int		ft_atoi(const char *nptr);
-void	*ft_memset(void *b, int c, size_t len);
-void	*ft_memcpy(void *dest, const void *src, size_t n);
-void	*ft_memchr(const void *s, int c, size_t n);
-int		ft_memcmp(const void *s1, const void *s2, size_t n);
-void	*ft_memmove(void *dst, const void *src, size_t len);
-void	ft_bzero(void *s, size_t n);
-size_t	ft_strlcat(char *dst, const char *src, size_t size);
-size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
-size_t	ft_strlen(const char *s);
-char	*ft_strnstr(const char *big, const char *little, size_t len);
-char	*ft_strrchr(const char *s, int c);
-int		ft_strncmp(const char *str1, const char *str2, size_t n);
-int		ft_tolower(int c);
-int		ft_toupper(int c);
-void	*ft_calloc(size_t count, size_t size);
-char	*ft_strdup(const char *str);
-char	*ft_substr(char const *s, unsigned int start, size_t len);
-char	*ft_strjoin(char const *s1, char const *s2);
-char	*ft_strchr(const char *s, int c);
-char	*ft_strtrim(char const *s1, char const *set);
-char	**ft_split(char const *s, char c);
-char	*ft_itoa(int n);
-char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
-void	ft_striteri(char *s, void (*f)(unsigned int, char *));
-void	ft_putchar_fd(char c, int fd);
-void	ft_putstr_fd(char *s, int fd);
-void	ft_putendl_fd(char *s, int fd);
-void	ft_putnbr_fd(int n, int fd);
-t_list	*ft_lstnew(void *content);
-void	ft_lstadd_front(t_list **lst, t_list *new);
-int		ft_lstsize(t_list *lst);
-t_list	*ft_lstlast(t_list *lst);
-void	ft_lstadd_back(t_list **lst, t_list *new);
-void	ft_lstdelone(t_list *lst, void (*del)(void*));
-void	ft_lstclear(t_list **lst, void (*del)(void*));
-void	ft_lstiter(t_list *lst, void (*f)(void *));
-t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
-void	ft_free_matrix(char **matrix);
-char	**ft_matrixdup(char **matrix, int height);
+/* ------------------------------------------------------------------------- */
+/*                           Character Checking Functions                    */
+/* ------------------------------------------------------------------------- */
+int			ft_isalpha(int c);
+int			ft_isdigit(int c);
+int			ft_isalnum(int c);
+int			ft_isascii(int c);
+int			ft_isprint(int c);
+int			ft_isspace(int c);
+int			ft_tolower(int c);
+int			ft_toupper(int c);
+
+/* ------------------------------------------------------------------------- */
+/*                           String Manipulation                             */
+/* ------------------------------------------------------------------------- */
+size_t		ft_strlen(const char *s);
+size_t		ft_strlcpy(char *dst, const char *src, size_t dstsize);
+size_t		ft_strlcat(char *dst, const char *src, size_t size);
+char		*ft_strnstr(const char *big, const char *little, size_t len);
+char		*ft_strrchr(const char *s, int c);
+char		*ft_strchr(const char *s, int c);
+int			ft_strncmp(const char *str1, const char *str2, size_t n);
+char		*ft_strdup(const char *str);
+char		*ft_substr(char const *s, unsigned int start, size_t len);
+char		*ft_strjoin(char const *s1, char const *s2);
+char		*ft_strtrim(char const *s1, char const *set);
+char		**ft_split(char const *s, char c);
+char		*ft_itoa(int n);
+char		*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+void		ft_striteri(char *s, void (*f)(unsigned int, char *));
+
+/* ------------------------------------------------------------------------- */
+/*                           Memory Manipulation                             */
+/* ------------------------------------------------------------------------- */
+void		*ft_memset(void *b, int c, size_t len);
+void		*ft_memcpy(void *dest, const void *src, size_t n);
+void		*ft_memmove(void *dst, const void *src, size_t len);
+void		*ft_memchr(const void *s, int c, size_t n);
+int			ft_memcmp(const void *s1, const void *s2, size_t n);
+void		ft_bzero(void *s, size_t n);
+void		*ft_calloc(size_t count, size_t size);
+
+/* ------------------------------------------------------------------------- */
+/*                             Number Conversion                             */
+/* ------------------------------------------------------------------------- */
+int			ft_atoi(const char *nptr);
+
+/* ------------------------------------------------------------------------- */
+/*                           Output Functions                                */
+/* ------------------------------------------------------------------------- */
+void		ft_putchar_fd(char c, int fd);
+void		ft_putstr_fd(char *s, int fd);
+void		ft_putendl_fd(char *s, int fd);
+void		ft_putnbr_fd(int n, int fd);
+
+/* ------------------------------------------------------------------------- */
+/*                           Linked List Manipulation                        */
+/* ------------------------------------------------------------------------- */
+t_list		*ft_lstnew(void *content);
+void		ft_lstadd_front(t_list **lst, t_list *new);
+int			ft_lstsize(t_list *lst);
+t_list		*ft_lstlast(t_list *lst);
+void		ft_lstadd_back(t_list **lst, t_list *new);
+void		ft_lstdelone(t_list *lst, void (*del)(void *));
+void		ft_lstclear(t_list **lst, void (*del)(void *));
+void		ft_lstiter(t_list *lst, void (*f)(void *));
+t_list		*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
+
+/* ------------------------------------------------------------------------- */
+/*                           Matrix Manipulation                             */
+/* ------------------------------------------------------------------------- */
+void		ft_free_matrix(char **matrix);
+char		**ft_matrixdup(char **matrix, int height);
 
 #endif
